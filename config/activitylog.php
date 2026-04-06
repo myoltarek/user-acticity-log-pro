50af536cbb7ddd837061db6f8cc7499e872d6bac
myoltarek <tarekmyolbd@gmail.com>
2026-04-06 18:00:05 +0600

    update activity log

--- config/activitylog.php
+++ config/activitylog.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    'log_methods' => ['POST', 'PUT', 'PATCH', 'DELETE'], // Methods
-    'log_guest_actions' => true, // guest actions log
+    'ignore_fields' => [
+        'password',
+        'remember_token'
+    ]
 ];
